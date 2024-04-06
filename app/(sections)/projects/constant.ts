a176b80988a6cc5c4e1b88376d98283c98800da3
pktnetclues <pthakur.netclues@gmail.com>
2024-04-06 19:38:21 +0530

    Update project descriptions and add missing image import in projects page

--- app/(sections)/projects/constant.ts
+++ app/(sections)/projects/constant.ts
@@ -1,18 +1,24 @@
 export const ProjectData = [
   {
     title: "Job Portal",
+    description:
+      "A job portal where users can search for jobs and apply for them.",
     techstack: ["React", "Redux", "Node", "Express", "MongoDB"],
     live: "https://findyourjob.vercel.app",
     github: "https://github.com/pankajktech/FindYourJob",
   },
   {
     title: "AI Article Summariser",
+    description:
+      "An AI article summariser that uses an external API to summarize articles.",
     techstack: ["React", "Redux", "Article Summarizer API"],
     live: "https://summarizearticle.netlify.app/",
     github: "https://github.com/pankajktech/ai-summariser",
   },
   {
     title: "MERN Blog App",
+    description:
+      "A blog website where users can read, write, and delete blogs.",
     techstack: ["React", "Node", "Express", "MongoDB", "Tailwind CSS"],
     live: "https://pankajktechblog.vercel.app/",
     github: "https://github.com/pankajktech/Blog-Website",
--- app/(sections)/projects/page.tsx
+++ app/(sections)/projects/page.tsx
@@ -2,7 +2,6 @@
 
 import { Github, LucideExternalLink } from "lucide-react";
 import Link from "next/link";
-import Image from "next/image";
 import { motion } from "framer-motion";
 
 import { ProjectData } from "./constant";
@@ -16,46 +15,49 @@ function ProjectsPage() {
         Projects
       </h1>
       <div className="flex  lg:max-w-[80%] flex-wrap justify-center gap-10">
-        {ProjectData.map(({ title, live, github, techstack }, index) => (
-          <Card
-            key={index}
-            className="flex flex-col max-md:w-[95%] gap-2 rounded-lg p-4 hover:outline-1 hover:outline-dashed "
-          >
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5 }}
-              className="flex flex-col md:flex-row items-center gap-4 justify-center"
+        {ProjectData.map(
+          ({ title, live, description, github, techstack }, index) => (
+            <Card
+              key={index}
+              className="flex flex-col max-md:w-[95%] gap-2 rounded-lg p-4 outline-1 outline-dashed "
             >
-              <h3 className="text-xl text-center font-medium">{title}</h3>
-              <div className="flex gap-2">
-                <Link href={live} target="_blank">
-                  <Button variant="outline">
-                    <LucideExternalLink size={15} />
-                  </Button>
-                </Link>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5 }}
+                className="flex flex-col md:flex-row items-center gap-4 justify-between"
+              >
+                <h3 className="text-xl text-center font-medium">{title}</h3>
+                <div className="flex gap-2">
+                  <Link href={live} target="_blank">
+                    <Button variant="outline">
+                      <LucideExternalLink size={15} />
+                    </Button>
+                  </Link>
 
-                <Link href={github} target="_blank">
-                  <Button variant="secondary">
-                    <Github size={15} />
-                  </Button>
-                </Link>
-              </div>
-            </motion.div>
-            <motion.p
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 0.3, duration: 0.5 }}
-              className="text-sm"
-            >
-              {techstack.map((tech, index) => (
-                <Button key={index} variant="outline" className="ml-2">
-                  {tech}
-                </Button>
-              ))}
-            </motion.p>
-          </Card>
-        ))}
+                  <Link href={github} target="_blank">
+                    <Button variant="secondary">
+                      <Github size={15} />
+                    </Button>
+                  </Link>
+                </div>
+              </motion.div>
+              <p className="text-sm text-center">{description}</p>
+              <motion.p
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.3, duration: 0.5 }}
+                className="text-sm flex flex-wrap justify-center gap-2"
+              >
+                {techstack.map((tech, index) => (
+                  <p key={index} className="border  p-1 rounded-md">
+                    {tech}
+                  </p>
+                ))}
+              </motion.p>
+            </Card>
+          )
+        )}
       </div>
     </div>
   );
