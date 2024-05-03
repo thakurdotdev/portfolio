1611bfcbcd90250a15335d22ad17cb0d38725f86
pankaj <pankajkatech@gmail.com>
2024-05-03 22:28:18 +0530

    chore: Update analytics package to latest version

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Navigation from "@/components/Navigation";
 import Social from "@/components/Social";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 
 
@@ -33,7 +33,7 @@ export default function RootLayout({
             <div className="hidden md:block absolute top-0 right-4 size-[500px] bg-fuchsia-200 mix-blend-multiply rounded-full filter blur-2xl opacity-50 animate-blob animation-delay-2000 -z-10"></div>
             <div className="absolute bottom-20 left-20 size-[500px] bg-pink-200 rounded-full mix-blend-multiply filter blur-2xl opacity-50 animate-blob animation-delay-4000 -z-10"></div>
             <div className=" hidden md:block absolute -bottom-8 right-1/4 size-[500px] bg-green-200 rounded-full mix-blend-multiply filter blur-2xl opacity-50 animate-blob animation-delay-4000 -z-10"></div>
-            <Analytics mode="production"/>
+            <Analytics/>
             {children}
             <Navigation />
             <Social />
