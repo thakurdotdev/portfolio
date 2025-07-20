d55b06e3cb70e8919a39d123c8c76394b065bb2f
Pankaj Thakur <pankaj@thakur.dev>
2025-07-20 16:33:37 +0530

    new newspaper style design

--- src/lib/db.ts
+++
@@ -1,7 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema";
-
-const connectionString = process.env.DATABASE_URL!;
-const client = postgres(connectionString);
-export const db = drizzle(client, { schema });
